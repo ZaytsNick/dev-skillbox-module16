9ebedf76f751dc688a27a8ff33b6eb8a8f30c2e5
Zayts Work <zaytsev@un74.ru>
2023-11-10 14:55:03 +0500

    task#1 complite

--- task#1/main1.cpp
+++ task#1/main1.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 int main()
 {
-    
+
+    float delta, speed(0);
+    do
+    {
+
+        char speed_str[5];
+        cout << "\nSpeed delta: ";
+        cin >> delta;
+        speed += delta;
+        sprintf(speed_str, "%.1f", speed);
+        cout << "\nSpeed: " << speed_str;
+    } while (delta + 0.01 > 0);
 }
