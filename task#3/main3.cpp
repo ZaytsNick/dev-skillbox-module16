6c77e93b5cf27bcf76b9242a8653498df662adcf
ZaytsNick <10kcin01@gmail.com>
2023-11-15 01:00:00 +0500

    task#3 complite

--- task#3/main3.cpp
+++ task#3/main3.cpp
@@ -1,42 +1,32 @@
 #include <iostream>
-#include <string>
+#include <sstream>
+// #include <string>
 
 using namespace std;
 
 int main()
 {
-    enum action
+    string buffer;
+    cout<<"Enter expression: ";
+    cin >> buffer;
+    double a, b;
+    char operation;
+    stringstream stream(buffer);
+    stream >> a >> operation >> b;
+    cout << a << operation << b << "=";
+    switch (operation)
     {
-        addition = '+',
-        subtraction = '-',
-        multiplication = '*',
-        division = '/'
-    };
-    string condition, Snumber1(""), Snumber2("");
-    int act(0);
-    cout << "Enter expression: ";
-    cin >> condition;
-    for (int i = 0; i < condition.size(); i++)
-    {
-        if (condition[i] == action::addition || action::division || action::multiplication || action::subtraction)
-        {
-            act = i;
-            continue;
-        }
-        else if (!act)
-            Snumber1 += condition[i];
-        if (act)
-            Snumber1 += condition[i];
+    case '+':
+        cout << a + b;
+        break;
+    case '-':
+        cout << a + b;
+        break;
+    case '*':
+        cout << a + b;
+        break;
+    case '/':
+        cout << a + b;
+        break;
     }
-
-    double number1 = stod(Snumber1);
-    double number2 = stod(Snumber2);
-    if (condition[act] == action::addition)
-        cout << number1 << action::addition << number2 << "=" << number1 + number2;
-    else if (condition[act] == action::division)
-        cout << number1 << action::division << number2 << "=" << number1 + number2;
-    else if (condition[act] == action::multiplication)
-        cout << number1 << action::multiplication << number2 << "=" << number1 + number2;
-    else if (condition[act] == action::subtraction)
-        cout << number1 << action::subtraction << number2 << "=" << number1 + number2;
 }
